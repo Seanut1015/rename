3e2b407c11caf1aca7c2d233f8fae7e49052bccb
Seanut1015 <56992986+Seanut1015@users.noreply.github.com>
2023-10-07 00:56:31 +0800

    1.1.0
    reduce size

---
+++ rename.py
@@ -0,0 +1,54 @@
+from subprocess import run
+import sys
+from PyQt5.QtWidgets import QFileDialog, QWidget, QApplication
+from UI import Ui_Form
+
+
+class MyWindow(QWidget, Ui_Form):
+    def __init__(self, parent=None):
+        super(MyWindow, self).__init__(parent)
+        self.setupUi(self)
+
+    def choose_path(self):
+        filePath = QFileDialog.getExistingDirectory()
+        filePath = "".join(filePath)
+        self.label.setText('"' + filePath + '"')
+
+    def format1_change(self, text):
+        self.lineEdit.setText(text)
+
+    def format2_change(self, text):
+        self.lineEdit_2.setText(text)
+
+    def rename(self):
+        name = self.lineEdit_3.text()
+        format1 = self.lineEdit.text()
+        format2 = self.lineEdit_2.text()
+        path = self.label.text()
+        if path == "Path":
+            return
+        commond_cd = '"cd ' + path
+        commond = (
+            "Get-ChildItem *."
+            + format1
+            + ' | ForEach-Object -Begin {;\
+        $count = 1;\
+        } -Process {;\
+        Rename-Item $_ -NewName \\"'
+            + name
+            + "$count."
+            + format2
+            + '\\";\
+        $count++ ;\
+        }'
+        )
+        commond = str(commond)
+        run("powershell " + commond_cd + ";" + commond + '"', shell=True)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ui = MyWindow()
+    ui.setWindowTitle("Rename")
+    ui.show()
+    sys.exit(app.exec_())
